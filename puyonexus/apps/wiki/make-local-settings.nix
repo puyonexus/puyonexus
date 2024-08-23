13b250795687c65f0be86f1276faf09a049778ec
John Chadwick <john@jchw.io>
2024-08-23 18:51:01 -0400

    Add Mediawiki permissions

--- puyonexus/apps/wiki/make-local-settings.nix
+++ puyonexus/apps/wiki/make-local-settings.nix
@@ -49,6 +49,43 @@ in
   $wgMaxImageArea = 50000000;
   $wgPFEnableStringFunctions = true;
 
+  # Permissions
+  $wgLogRestrictions["newusers"] = 'moderation';
+  $wgAddGroups['sysop'][] = 'automoderated';
+  $wgRemoveGroups['sysop'][] = 'automoderated';
+  $wgGroupPermissions['*']['move'] = false;
+  $wgGroupPermissions['*']['edit'] = false;
+  $wgGroupPermissions['*']['createaccount'] = true;
+  $wgGroupPermissions['*']['abusefilter-log-detail'] = true;
+  $wgGroupPermissions['*']['abusefilter-view'] = true;
+  $wgGroupPermissions['*']['abusefilter-log'] = true;
+  $wgGroupPermissions['user']['move'] = false;
+  $wgGroupPermissions['user']['edit'] = true;
+  $wgGroupPermissions['user']['move-categorypages'] = false;
+  $wgGroupPermissions['user']['movefile'] = false;
+  $wgGroupPermissions['user']['move-subpages'] = false;
+  $wgGroupPermissions['user']['move-rootuserpages'] = false;
+  $wgGroupPermissions['user']['sendemail'] = false;
+  $wgGroupPermissions['bot']['skip-moderation'] = true;
+  $wgGroupPermissions['checkuser']['moderation-checkuser'] = false;
+  $wgGroupPermissions['automoderated']['move'] = true;
+  $wgGroupPermissions['automoderated']['move-categorypages'] = true;
+  $wgGroupPermissions['automoderated']['movefile'] = true;
+  $wgGroupPermissions['automoderated']['move-subpages'] = true;
+  $wgGroupPermissions['automoderated']['move-rootuserpages'] = true;
+  $wgGroupPermissions['automoderated']['sendemail'] = true;
+  $wgGroupPermissions['automoderated']['move'] = true;
+  $wgGroupPermissions['sysop']['abusefilter-modify'] = true;
+  $wgGroupPermissions['sysop']['moderation'] = true;
+  $wgGroupPermissions['sysop']['skip-moderation'] = true;
+  $wgGroupPermissions['sysop']['checkuser'] = true;
+  $wgGroupPermissions['sysop']['checkuser-log'] = true;
+  $wgGroupPermissions['sysop']['abusefilter-private'] = true;
+  $wgGroupPermissions['sysop']['abusefilter-modify-restricted'] = true;
+  $wgGroupPermissions['sysop']['abusefilter-revert'] = true;
+  $wgGroupPermissions['sysop']['editsitecss'] = true;
+  $wgGroupPermissions['sysop']['editsitejs'] = true;
+
   # Database
   $wgDBtype = 'mysql';
   $wgDBserver = ${mkNowDoc mysqlServer};
