eb1c6fcc6ee1c05207551404767da10f5861162c
John Chadwick <john@jchw.io>
2025-04-11 00:04:52 -0400

    Reduce memory usage in staging

--- modules/environment/staging/default.nix
+++ modules/environment/staging/default.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ lib, config, ... }:
 {
   imports = [ ./.. ];
 
@@ -13,5 +13,16 @@
     # Use external MTA in staging
     puyonexus.mail.externalMta.enable = true;
     puyonexus.mail.domain = "sandbox2cad93530245423fa4399fafb87f1f5e.mailgun.org";
+
+    # Reduce memory usage in staging
+    services.phpfpm.pools.www.settings = {
+      "pm.max_children" = lib.mkForce 10;
+      "pm.start_servers" = lib.mkForce 1;
+      "pm.min_spare_servers" = lib.mkForce 1;
+      "pm.max_spare_servers" = lib.mkForce 3;
+    };
+    services.redis.servers.wiki.settings = {
+      maxmemory = lib.mkForce "128mb";
+    };
   };
 }
