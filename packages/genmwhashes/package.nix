1d8c3478379c42f25301df2ee74d861b6cffdd22
John Chadwick <john@jchw.io>
2025-07-08 00:51:42 -0400

    Format genmwhashes package.nix

--- packages/genmwhashes/package.nix
+++ packages/genmwhashes/package.nix
@@ -1,7 +1,14 @@
-{ writeShellApplication, nix-prefetch-git, jq }:
+{
+  writeShellApplication,
+  nix-prefetch-git,
+  jq,
+}:
 
 writeShellApplication {
   name = "genmwhashes";
   text = builtins.readFile ./genmwhashes.sh;
-  runtimeInputs = [ nix-prefetch-git jq ];
+  runtimeInputs = [
+    nix-prefetch-git
+    jq
+  ];
 }
