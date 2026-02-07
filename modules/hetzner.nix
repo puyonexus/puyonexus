9ba31a97e4af174301199f15f3e5d0de26af53f8
John Chadwick <john@jchw.io>
2026-02-07 17:02:12 -0500

    Split disko config for simplicity

--- modules/hetzner.nix
+++ modules/hetzner.nix
@@ -3,64 +3,13 @@
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
+    ./disko.nix
   ];
 
   config = {
     boot.loader.grub = {
       efiSupport = true;
       efiInstallAsRemovable = true;
-    };
-    disko.devices = {
-      disk.disk1 = {
-        device = lib.mkDefault "/dev/sda";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              name = "boot";
-              size = "1M";
-              type = "EF02";
-            };
-            esp = {
-              name = "ESP";
-              size = "1G";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            root = {
-              name = "root";
-              size = "100%";
-              content = {
-                type = "lvm_pv";
-                vg = "pool";
-              };
-            };
-          };
-        };
-      };
-      lvm_vg = {
-        pool = {
-          type = "lvm_vg";
-          lvs = {
-            root = {
-              size = "100%FREE";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-                mountOptions = [
-                  "defaults"
-                ];
-              };
-            };
-          };
-        };
-      };
     };
   };
 }
