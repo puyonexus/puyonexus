9bfc3784df4a9bcf2f8cc87d8802aec43f3c833a
John Chadwick <john@jchw.io>
2026-02-16 15:33:20 -0500

    Update nix formatting

--- modules/puyonexus/wiki/default.nix
+++ modules/puyonexus/wiki/default.nix
@@ -196,7 +196,7 @@ in
         Restart = "always";
         RestartSec = "1min";
         Type = "exec";
-        ExecStart = ''${
+        ExecStart = "${
           pkgs.writeShellApplication {
             name = "mwjobrunner";
             runtimeInputs = [ config.puyonexus.php.package ];
@@ -210,7 +210,7 @@ in
               done
             '';
           }
-        }/bin/mwjobrunner'';
+        }/bin/mwjobrunner";
       };
     };
   };
